6806c25a3f5496370d4741d6000655446ca47b93
kidszoneWorld <ti.kz@kidszoneworld.com.br>
2025-10-29 08:41:31 -0300

    Delete models directory

--- models/Cliente.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ClienteSchema = new mongoose.Schema({
-    codigo_cliente: { type: String, required: true },
-    nome: { type: String, required: true },
-    representante:Number
-});
-
-module.exports = mongoose.model('Cliente', ClienteSchema);
