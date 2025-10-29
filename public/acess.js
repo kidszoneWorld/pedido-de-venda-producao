5b2aad6398ff49515fbfe6ff8cab78e194ac4d1c
kidszoneWorld <ti.kz@kidszoneworld.com.br>
2025-10-29 08:41:39 -0300

    Delete public directory

--- public/acess.js
+++
@@ -1,5 +0,0 @@
-// config.js (com hash da senha)
-const credentials = {
-    login: "ti.kz@kidszoneworld.com.br",
-    passwordHash: "7d35f3180d46ab772a48db9ca2775bcedd7cf34c55333e49095e8bd750327bb2" // hash da senha
-};
